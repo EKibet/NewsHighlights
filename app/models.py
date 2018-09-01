1f64e4828e2e8447851ac4c5c438b71b005fe797
EKibet <kibetedgar@gmail.com>
2018-09-01 11:25:56 +0300

    Adds source class

---
+++ app/models.py
@@ -0,0 +1,8 @@
+class Source:
+
+    def __init__(self, id,name,description,category,country):
+        self.id = id
+        self.name = name
+        self.description = description
+        self.category = category
+        self.country = country
